3ff05ae4b2f3ed3763bf5aa61899bf9809dee5d2
monte <greg.jonca@gmail.com>
2018-10-14 13:20:25 +0200

    Improve policy deployment

--- week-3/homework/3-1/deploy-policy.sh
+++ week-3/homework/3-1/deploy-policy.sh
@@ -10,24 +10,25 @@ VMDISK_POLICY="vmdisk-naming-convention"
 VMDISK_ASSIGNMENT="check-vmdisk-naming-convention"
 
 STORAGE_POLICY="storage-naming-convention"
-STORAGE_ASSIGNMENT="chec-storage-naming-convention"
+STORAGE_ASSIGNMENT="check-storage-naming-convention"
 
 VM_POLICY="vm-naming-convention"
 VM_ASSIGNMENT="check-vm-naming-convention"
 
 
-
-az.cmd policy definition create --name $RG_POLICY --rules vnet-naming-convention-policy.json --mode All
+echo "Start executing az commands!"
+az.cmd policy definition create --name $RG_POLICY --rules rg-naming-convention-policy.json --mode All
 az.cmd policy assignment create --name $RG_ASSIGNMENT --policy $RG_POLICY
 
 az.cmd policy definition create --name $VNET_POLICY --rules vnet-naming-convention-policy.json --mode All
 az.cmd policy assignment create --name $VNET_ASSIGNMENT --policy $VNET_POLICY
 
-az.cmd policy definition create --name $VMDISK_POLICY --rules vnet-naming-convention-policy.json --mode All
+az.cmd policy definition create --name $VMDISK_POLICY --rules vmdisk-naming-convention-policy.json --mode All
 az.cmd policy assignment create --name $VMDISK_ASSIGNMENT --policy $VMDISK_POLICY
 
-az.cmd policy definition create --name $STORAGE_POLICY --rules vnet-naming-convention-policy.json --mode All
+az.cmd policy definition create --name $STORAGE_POLICY --rules storage-naming-convention-policy.json --mode All
 az.cmd policy assignment create --name $STORAGE_ASSIGNMENT --policy $STORAGE_POLICY
 
-az.cmd policy definition create --name $VM_POLICY --rules vnet-naming-convention-policy.json --mode All
+az.cmd policy definition create --name $VM_POLICY --rules vm-naming-convention-policy.json --mode All
 az.cmd policy assignment create --name $VM_ASSIGNMENT --policy $VM_POLICY
+echo "Stop executing az commands!"
