35543eb36b9248c895565a2ccac4e2b7f8e39127
monte <greg.jonca@gmail.com>
2018-10-25 07:11:49 +0200

    Fix deploying vmss script

--- week-4/homework/vmss-deploy.sh
+++ week-4/homework/vmss-deploy.sh
@@ -6,5 +6,5 @@ VMSS_NAME=$3
 IMAGE=$4
 ADMIN=$5
 
-az.cmd group create --name RESOURCE_GROUP --location LOCATION
-az.cmd vmss create --resource-group RESOURCE_GROUP --name VMSS_NAME --image IMAGE --upgrade-policy-mode automatic --admin-username ADMIN
+az.cmd group create --name $RESOURCE_GROUP --location $LOCATION
+az.cmd vmss create --resource-group $RESOURCE_GROUP --name $VMSS_NAME --image $IMAGE --upgrade-policy-mode automatic --admin-username $ADMIN --generate-ssh-keys
