2971aea2b93cc0e2acac5719940becbf0a4f11d7
monte <greg.jonca@gmail.com>
2018-12-17 20:28:11 +0100

    Add simple implementation for week 11

---
+++ week-11/MediaApi/Controllers/ValuesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+
+namespace MediaApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediaController : ControllerBase
+    {
+        private readonly HttpClient _httpClient;
+
+        public MediaController(IConfiguration config)
+        {
+            _httpClient = new HttpClient();
+            _httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", $"{config["vision-api-key"]}");
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] string url)
+        {
+            _httpClient.BaseAddress = new Uri("https://westeurope.api.cognitive.microsoft.com/vision/v1.0/analyze");
+            var result = await _httpClient.PostAsync("?visualFeatures=Adult", new StringContent(JsonConvert.SerializeObject(new { url }), Encoding.UTF8, "application/json"));
+
+            var responseBody = await result.Content.ReadAsStringAsync();
+            dynamic deserializedResponse = JsonConvert.DeserializeObject(responseBody);
+            if ((bool)deserializedResponse.adult.isAdultContent)
+                return StatusCode(406, "Image is for adults!");
+            return Ok("Image is OK!");
+        }
+    }
+}
